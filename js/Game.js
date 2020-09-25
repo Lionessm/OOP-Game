da15120f895d26f5f630509fb580991e3626cb68
Maria <maria.mihaila.17@gmail.com>
2020-09-25 14:56:37 +0300

    Created Game object with createPhrases method and getRandomPhrase method. Populated the Game object phrases array with the phrases used in the game. Created Phrase object and declared methods addPhraseToDisplay, checkLetter, showMatchedLetter.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+
+    createPhrases(phrase) {
+        this.phrases.push(phrase)
+    }
+
+    getRandomPhrase( ) {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+        let randomPhraseIndex = getRandomInt(5);
+        return this.phrases[randomPhraseIndex];
+    }
+
+}
+
+const game = new Game();
+const phrase = new Phrase('Break a leg');
+const phrase2 = new Phrase('Success');
+const phrase3 = new Phrase('Bite your tongue');
+const phrase4 = new Phrase('Drop me a line');
+const phrase5 = new Phrase('Beats me');
+game.createPhrases(phrase);
+game.createPhrases(phrase2);
+game.createPhrases(phrase3);
+game.createPhrases(phrase4);
+game.createPhrases(phrase5);
+
