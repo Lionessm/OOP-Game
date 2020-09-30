befabbbcfefa72d11549125d5d09f61ee55bdaf4
Maria <maria.mihaila.17@gmail.com>
2020-09-30 19:33:26 +0300

    Edit of comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-
-// event listener for the "Start Game" button, that initiates the game by calling the startGame method
+// Event listener for the "Start Game" button, that initiates the game by calling the startGame method.
 document.getElementById('btn__reset').addEventListener( 'click' , (e) => {
     game.startGame();
 });
 
-// select keys of the keyboard
+// Select keys of the keyboard.
 let keyElements = document.querySelectorAll('button.key');
 
-// search for events in every key element
+// Search for events in every key element.
 for (let i = 0; i < keyElements.length; i++) {
     keyElements[i].addEventListener('click', (e) => {
         game.handleInteraction(e.target);
